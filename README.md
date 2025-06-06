e38df2301e5d31ffa8235d0840afb757bc30a2b3
Borja Sevilla <bsevrui@gmail.com>
2025-06-06 19:11:15 +0200

    initial deployment

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# gamebacklog-admin
+# GameBacklog Admin Panel
+
+## Versionado
+Versión: ```0.0.25.6.6```
+
+## Descripción
+Pandel de Administrador diseñado para el PFG de DAW, construida en _Ionic 8 (Angular)_.
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gamebacklog-admin',
+  webDir: 'www'
+};
+
+export default config;
