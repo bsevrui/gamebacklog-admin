e215e09b5822e43c938a1ce4d8f6f619fc5e8122
Borja Sevilla <bsevrui@gmail.com>
2025-06-07 00:18:27 +0200

    feat(users): completed

--- src/app/pages/user-module/update/update.page.ts
+++ src/app/pages/user-module/update/update.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonSelect, IonSelectOption, IonList, IonItem, IonButton, IonIcon, IonButtons, IonBackButton, IonLabel } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonSelect, IonSelectOption, IonList, IonItem, IonButton, IonIcon, IonButtons, IonBackButton, IonLabel, IonInput } from '@ionic/angular/standalone';
 import { ApiService } from 'src/app/core/services/api.service';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { User } from 'src/app/core/interfaces/user';
@@ -14,12 +14,17 @@ import { saveSharp, close, mail } from 'ionicons/icons';
   templateUrl: './update.page.html',
   styleUrls: ['./update.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, TranslateModule, FormsModule, RouterLink, IonSelect, IonSelectOption, IonButton, IonList, IonItem, IonIcon, IonBackButton, IonButtons, IonLabel]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, TranslateModule, FormsModule, RouterLink, IonSelect, IonSelectOption, IonButton, IonList, IonItem, IonIcon, IonBackButton, IonButtons, IonLabel, IonInput]
 })
 export class UpdatePage implements OnInit {
   private userId?: number;
   public user?: User;
-  public role?: string;
+  public username?: string;
+  public role?: 'ADMIN' | 'USER';
+  public birthdate?: Date;
+  public firstName?: string;
+  public lastName?: string;
+  public profilePicture?: string;
 
   /**
    * 
@@ -51,6 +56,23 @@ export class UpdatePage implements OnInit {
   }
 
   update() {
-    
+    if (this.userId) {
+      this.apiService.updateUser(this.userId, {
+        username: this.username,
+        role: this.role,
+        birthdate: this.birthdate,
+        firstName: this.firstName,
+        lastName: this.lastName,
+        profilePicture: this.profilePicture
+      }).subscribe({
+        next: (res) => {
+          console.log('updated: ', res);
+          this.router.navigate(['/users']).then(() => {
+            window.location.reload();
+          });
+        },
+        error: (err) => console.error('error: ', err)
+      });
+    }
   }
 }
