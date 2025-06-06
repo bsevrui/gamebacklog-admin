6235d48f0706bb1c82da1ab00b3b9bd7c7d27182
Borja Sevilla <bsevrui@gmail.com>
2025-06-06 21:47:04 +0200

    feat(user): added update page

--- src/app/pages/user-module/user.route.ts
+++ src/app/pages/user-module/user.route.ts
@@ -6,5 +6,10 @@ export const userRoutes: Routes = [
         path: '',
         canActivate: [authGuard],
         loadComponent: () => import('./user-module.page').then(m => m.UserModulePage)
+    },
+    {
+        path: 'update/:userId',
+        canActivate: [authGuard],
+        loadComponent: () => import('./update/update.page').then(m => m.UpdatePage)
     }
 ];
