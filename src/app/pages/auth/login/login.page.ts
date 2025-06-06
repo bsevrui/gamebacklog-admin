71dfa68d710215655da843be05c959a6b519f5ad
Borja Sevilla <bsevrui@gmail.com>
2025-06-06 19:20:52 +0200

    feat(login): initial deployment

--- src/app/pages/auth/login/login.page.ts
+++ src/app/pages/auth/login/login.page.ts
@@ -11,10 +11,7 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class LoginPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
