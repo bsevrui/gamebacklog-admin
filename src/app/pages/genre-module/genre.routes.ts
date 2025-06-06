1e1c07ae8016eaf49ab347317b26c7fccba55e4b
Borja Sevilla <bsevrui@gmail.com>
2025-06-06 21:46:32 +0200

    feat(genre): added update & add page

--- src/app/pages/genre-module/genre.routes.ts
+++ src/app/pages/genre-module/genre.routes.ts
@@ -6,5 +6,15 @@ export const genreRoutes: Routes = [
         path: '',
         canActivate: [authGuard],
         loadComponent: () => import('./genre-module.page').then(m => m.GenreModulePage)
+    },
+    {
+        path: 'add',
+        canActivate: [authGuard],
+        loadComponent: () => import('./add/add.page').then(m => m.AddPage)
+    },
+    {
+        path: 'update/:genreId',
+        canActivate: [authGuard],
+        loadComponent: () => import('./update/update.page').then(m => m.UpdatePage)
     }
 ];
