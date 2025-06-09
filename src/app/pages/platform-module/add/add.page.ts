e901454e3c240b12ab6c8893a42ad0609b1c4711
Borja Sevilla <bsevrui@gmail.com>
2025-06-09 14:02:21 +0200

    feat(platform/add): completed

--- src/app/pages/platform-module/add/add.page.ts
+++ src/app/pages/platform-module/add/add.page.ts
@@ -2,17 +2,94 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/angular/standalone';
+import { ApiService } from 'src/app/core/services/api.service';
+import { LocalizationService } from 'src/app/core/services/localization.service';
+import { RouterLink, Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { CreatePlatform } from 'src/app/core/interfaces/platform/create-platform';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton , IonList, IonItem, IonInput, IonTextarea, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { saveSharp, close } from 'ionicons/icons';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.page.html',
   styleUrls: ['./add.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TranslateModule, IonButtons, IonBackButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TranslateModule, RouterLink, IonButtons, IonBackButton, IonList, IonItem, IonInput, IonTextarea, IonButton, IonIcon]
 })
 export class AddPage implements OnInit {
-  constructor() {}
+  public name?: string;
+  public releaseDate?: Date;
+  public detail?: string;
+  public picture?: string;
+
+  /**
+   * Constructor
+   * @param apiService API Service
+   * @param localizationService Localization Service
+   * @param router Router
+   * @param toastCtrl Toast Controller
+   */
+  constructor(
+    private apiService: ApiService,
+    private localizationService: LocalizationService,
+    private router: Router,
+    private toastCtrl: ToastController
+  ) {
+    addIcons({ saveSharp, close });
+  }
 
   ngOnInit() {}
+
+  async presentToast(msg: string, color: string) {
+    const toast = await this.toastCtrl.create({
+      message: msg,
+      duration: 2000,
+      position: 'bottom',
+      color: color
+    });
+    await toast.present();
+  }
+
+  create() {
+    if (this.name && this.releaseDate) {
+      const platformData: CreatePlatform = {
+        name: this.name,
+        releaseDate: this.releaseDate,
+        detail: this.detail,
+        picture: this.picture
+      };
+
+      this.apiService.createPlatform(platformData).pipe(
+        catchError((error) => {
+          this.localizationService.translate(['WARNING_GENERIC', 'WARNING_PLATFORM_ALREADYCREATED']).subscribe(async (values) => {
+            let errorMsg = values['WARNING_GENERIC'];
+
+            if (error?.error?.message == 'platform already registered') {
+              errorMsg = values['WARNING_PLATFORM_ALREADYCREATED'];
+            }
+
+            this.presentToast(errorMsg, 'danger');
+          });
+          return of(null);
+        })
+      ).subscribe((res) => {
+        if (res) {
+          this.localizationService.translate(['TOAST_PLATFORM_CREATED']).subscribe(async (values) => {
+            this.router.navigate(['/platforms']).then(() => {
+              window.location.reload();
+            });
+            await this.presentToast(values['TOAST_PLATFORM_CREATED'], 'success');
+          });
+        }
+      })
+    } else {
+      this.localizationService.translate(['WARNING_MANDATORY_FIELDS']).subscribe(async (values) => {
+        this.presentToast(values['WARNING_MANDATORY_FIELDS'], 'warning');
+      });
+    }
+  }
 }
